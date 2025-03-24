e5cfe59e7bd89a95f52b88ffa4b2aec2d50448c4
Anca <negulescuanca11@gmail.com>
2025-03-24 21:54:54 +0100

    Create migration using php bin/console make:migration

---
+++ migrations/Version20250324205408.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250324205408 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE temperature_datasets (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, calculated_mkt DOUBLE PRECISION DEFAULT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE temperature_records (id INT AUTO_INCREMENT NOT NULL, dataset_id INT NOT NULL, time DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', temperature DOUBLE PRECISION NOT NULL, INDEX IDX_9EF9B774D47C2D1B (dataset_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE temperature_records ADD CONSTRAINT FK_9EF9B774D47C2D1B FOREIGN KEY (dataset_id) REFERENCES temperature_datasets (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE temperature_records DROP FOREIGN KEY FK_9EF9B774D47C2D1B');
+        $this->addSql('DROP TABLE temperature_datasets');
+        $this->addSql('DROP TABLE temperature_records');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
